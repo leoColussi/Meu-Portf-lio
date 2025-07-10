d30e153c23446d7c86576ad5a269f8c1fc854441
Leonardo Colussi <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>
2025-07-09 23:26:48 -0300

    :tada: Commit Inicial

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function escrevendoLetra() {
+    
+    function ativaLetra(elemento) {
+        const arrtexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrtexto.forEach((letra, i) => {
+            setTimeout(() => {
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
+
+
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
+}
+
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria');
+                                /* AIR FUNCTION */
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
+})
+
